af16825a1ed121b5c3bd0df1e7c78ea51bdcba80
Sergei Bilardi <bilardis@my.erau.edu>
2017-04-21 11:42:41 -0400

    Update commonHeaders.h

--- CodeFiles/commonHeaders.h
+++ CodeFiles/commonHeaders.h
@@ -4,4 +4,9 @@
 #define F_CPU 1000000UL
 #include <util/delay.h>
 
+/* Determine which direction system should move */
+#define GOCOUNTERCLOCK  0
+#define IDLE            1
+#define GOCLOCKWISE     2
+
 #define DEBUG 0
