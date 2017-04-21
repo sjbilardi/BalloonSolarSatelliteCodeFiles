dcbfdb1c5412435142fae21c6516167a29430a7c
Sergei Bilardi <bilardis@my.erau.edu>
2017-04-21 11:28:12 -0400

    Update LEDComm.h

--- CodeFiles/LEDComm.h
+++ CodeFiles/LEDComm.h
@@ -1,3 +1,23 @@
 #include "commonHeaders.h"
 
+/* LED */
+typedef struct {
+	uint16_t val[3];
+  
+  char leftLED;
+  char middleLED;
+  char rightLED;
+	
+	char pins[3];
+	} LEDS;
 
+/* Motor Functions */
+LEDS leds_init(char rightLEDpin, char middleLEDpin, char rightLEDpin)
+{
+	LEDS leds = {	{0, 0, 0},  // default val to zero
+					0, // left LED index
+					1, // middle LED index
+          2, // right LED index
+          {rightLEDpin, middleLEDpin, rightLEDpin}}; // 
+	return leds;
+}
