59624862a629a76c2a84632f781d20167c6583fb
Sergei Bilardi <bilardis@my.erau.edu>
2017-04-28 21:08:43 -0400

    Update voltageProbes.h

--- CodeFiles/voltageProbes.h
+++ CodeFiles/voltageProbes.h
@@ -14,15 +14,16 @@ typedef struct {
 
 VoltageProbes voltageProbes_init(char pin1, char pin2)
 {
-	VoltageProbes voltageProbes = {	{0, 0},
-									{0.0, 0.0}, // V
-									0.0, // mA
-									5.1, // k-ohms
-									5.1, // k-ohms
-									1,	 // ohms
-									0,	 // battery V index
-									1,	 // resistor V index
-									{pin1, pin2}};
+	VoltageProbes voltageProbes = {	
+		{0, 0},
+		{0.0, 0.0},	// V
+		0.0,		// mA
+		5.1,		// k-ohms
+		5.1,		// k-ohms
+		1,		// ohms
+		0,		// battery V index
+		1,		// resistor V index
+		{pin1, pin2}};
 	return voltageProbes;
 }
 
@@ -34,11 +35,13 @@ void readVoltageCurrent(VoltageProbes *voltageProbes)
 	for(i=0; i<sizeof(voltageProbes->adcVal)/sizeof(uint16_t); i++)
 	{
 		voltageProbes->adcVal[i] = readAnalog(voltageProbes->pin[i]);
-		voltageProbes->voltage[i] = (((double)voltageProbes->adcVal[i]/1024)) * VCC * 
-									(voltageProbes->R + voltageProbes->RL)/voltageProbes->RL; // V
+		voltageProbes->voltage[i] = (((double)voltageProbes->adcVal[i]/1024)) * 
+					VCC * (voltageProbes->R + 
+					voltageProbes->RL)/voltageProbes->RL; // V
 	}
 
 	/* Calculate the current using the voltage values */
-	voltageProbes->current = (fabs(voltageProbes->voltage[0] - voltageProbes->voltage[1]) / 
-							 voltageProbes->currentResistor); // mA
+	voltageProbes->current = (fabs(voltageProbes->voltage[0] - 
+					voltageProbes->voltage[1]) / 
+					voltageProbes->currentResistor); // mA
 }
